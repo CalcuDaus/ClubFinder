f96590db4aaf59262971fcb905b7ada56269a995
daus <dausg878@gmail.com>
2023-12-23 18:53:18 +0700

    update using component

--- src/script/components/app-bar.js
+++ src/script/components/app-bar.js
@@ -2,9 +2,10 @@ class AppBar extends HTMLElement {
   connectedCallback() {
     this.render();
   }
+ 
   render() {
     this.innerHTML = `<h2>Club Finder</h2>`;
   }
 }
-
-customElements.define("app-bar", AppBar);
+ 
+customElements.define('app-bar', AppBar);
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,46 +1,24 @@
 // import data source
-import '../components/app-bar.js';
 import '../components/search-bar.js';
 import '../components/club-list.js';
 import DataSource from "../data/data-source.js";
 
 const main = () => {
-  const header = document.querySelector('header');
-  const main = document.querySelector('main');
-  const clubListElement = document.createElement("club-list");
+  const clubListElement = document.querySelector("club-list");
+  const searchElement = document.querySelector("search-bar");
 
-
-  // render app bar 
-  const renderAppBar = ()=>{
-    const appBarElement = document.createElement('app-bar');
-    header.appendChild(appBarElement);
-  }
-  renderAppBar();
-
-  // render search bar
-  const renderSearchBar = ()=>{
-    const searchBarElement = document.createElement('search-bar');
-    main.appendChild(searchBarElement);
-  }
-  renderSearchBar();
-  const searchElement = document.querySelector("#searchElement");
-  const searchButtonElement = document.querySelector('#searchButtonElement');
   const onButtonSearchClicked = () => {
     async function data(searchElement) {
       try {
-        clubListElement.innerHTML = "";
         const clubResults = await DataSource.searchClub(searchElement);
         clubListElement.clubs = clubResults;
-        main.appendChild(clubListElement);
       } catch (rejected) {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${rejected}</h2>`;
-        main.appendChild(clubListElement);
+        clubListElement.renderError(rejected);
       }
     }
     data(searchElement.value);
   };
-  searchButtonElement.addEventListener('click',()=>{onButtonSearchClicked();})
+  searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
