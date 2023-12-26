c0d33476d1b85423fdd5dba19b860432f620a931
daus <dausg878@gmail.com>
2023-12-26 16:37:37 +0700

    menambahkan babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,19 @@ module.exports = {
                         loader: 'css-loader'
                     }
                 ]
+            },
+            // babel loader
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader: 'babel-loader',
+                        options:{
+                            presets:['@babel/preset-env']
+                        }
+                    }
+                ]
             }
         ]
     }
